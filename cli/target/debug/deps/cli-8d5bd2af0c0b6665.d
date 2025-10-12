5fabfcf3d87153ffe933dcc664316a9eaa312f2f
Warre Dujardin <warre.dujardin@gmail.com>
2025-10-12 15:41:58 +0200

    init

---
+++ cli/target/debug/deps/cli-8d5bd2af0c0b6665.d
@@ -0,0 +1,5 @@
+/home/warre/Repositories/renderers/cli/target/debug/deps/cli-8d5bd2af0c0b6665.d: src/main.rs
+
+/home/warre/Repositories/renderers/cli/target/debug/deps/libcli-8d5bd2af0c0b6665.rmeta: src/main.rs
+
+src/main.rs:
---
+++ cli/target/debug/deps/cli-d921c99c15faea1e.d
@@ -0,0 +1,5 @@
+/home/warre/Repositories/renderers/cli/target/debug/deps/cli-d921c99c15faea1e.d: src/main.rs
+
+/home/warre/Repositories/renderers/cli/target/debug/deps/libcli-d921c99c15faea1e.rmeta: src/main.rs
+
+src/main.rs:
